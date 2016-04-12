542904e00a7994b411dd2cd40a0e4b74b720fca4
davinod <davinod@gmail.com>
2016-04-11 23:05:12 -0300

    First Version of Quiz

---
+++ app/src/main/java/com/bignerdcradt/android/geoquiz/Question.java
@@ -0,0 +1,30 @@
+package com.bignerdcradt.android.geoquiz;
+
+/**
+ * Created by Davi_Diogo on 4/11/2016.
+ */
+public class Question {
+    private int mTextResId ;
+    private boolean mAnswerTrue;
+
+    public Question (int textResId, boolean answerTrue){
+        setTextResId(textResId);
+        setAnswerTrue(answerTrue);
+    }
+
+    public int getTextResId() {
+        return mTextResId;
+    }
+
+    public void setTextResId(int textResId) {
+        mTextResId = textResId;
+    }
+
+    public boolean isAnswerTrue() {
+        return mAnswerTrue;
+    }
+
+    public void setAnswerTrue(boolean answerTrue) {
+        mAnswerTrue = answerTrue;
+    }
+}
